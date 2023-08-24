de2066b65fbeff83e32e35a4dcfcc6608c1cd47d
M Asad <ma6530028@gmail.com>
2023-08-24 05:34:35 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const showModal = new bootstrap.Modal(document.getElementById("AddModal"));
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const shModal = new bootstrap.Modal(document.getElementById("ShowModal"));
-
+const editModal = new bootstrap.Modal(document.getElementById("editModal"));
 
 const handler = new Datahandler();
 let category = handler.GetCategories();
@@ -53,38 +53,38 @@ function LoadCards(tasks) {
   document.getElementById("tblbody").innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
     let temp = `
-        <div class=" col-md-6 col-lg-4">
-          <div class="card mt-4">
-            <div class="m-4" >
-                  <h3 class="mb-0"><a href="#" class="link-show text-decoration-none text-dark" data-vs-pd="${tasks[i].Title},${tasks[i].Detail},${tasks[i].Category.Id}">  ${tasks[i].Title}</a></h3>
-                  <div class=" text-body-secondary">${tasks[i].PostedOn}</div>
+    <div class="col-md-6 col-lg-4">
+        <div class="card mt-4">
+            <div class="m-4">
+                <h3 class=" link-show" data-vs-pd="${tasks[i].Title},${tasks[i].Detail},${tasks[i].Category.Id}">
+                    ${tasks[i].Title}
+                </h3>
+                <div class="text-body-secondary">${tasks[i].PostedOn}</div>
+            </div>
+            <div class="card-body">
+                <div class="col d-flex flex-column position-static">
+                    <p class="card-text mb-auto">${tasks[i].Detail}</p>
+                    <div class="d-flex top">
+                        
+                        <i class="fa-regular fa-trash-can fa-lg text-danger delete-Card" id="${tasks[i].Id}"></i>
+                        &nbsp;&nbsp;&nbsp;
+                        <i class="fa-regular fa-pen-to-square fa-lg text-success edit-Card" id="${tasks[i].Id}"></i>
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>`;
 
-              <div class="card-body">
-                  <div class="col  d-flex flex-column position-static">
-                      <p class="card-text mb-auto">${tasks[i].Detail}</p>
-                      <div class="d-flex top">
 
-                              <i class="fa-regular fa-star fa-lg text-primary favourite" id="${tasks[i].Id}"></i>
-                          &nbsp;&nbsp;&nbsp;
-
-                              <i class="fa-regular fa-trash-can fa-lg text-danger delete-Card" id="${tasks[i].Id}" ></i>
-
-                      </div>
-                  </div>
-              </div>
-          </div>
-        </div>`;
     document.getElementById("tblbody").innerHTML += temp;
   }
 
   document.querySelectorAll(".delete-Card").forEach((icon) => {
     icon.addEventListener("click", function (e) {
+      e.stopPropagation(); // Stop event propagation
       // body
       const pid = e.target.getAttribute("id");
-      // console.log(pid);
       const found = handler.FindProducts(pid);
-      // console.log(found);
       if (found) {
         document.getElementById("delhdnid").value = found.Id;
         document.getElementById("deltxttitle").value = found.Title;
@@ -96,70 +96,93 @@ function LoadCards(tasks) {
     });
   });
 
-document.querySelectorAll('.link-show').forEach((lnk)=>{ 
-    lnk.addEventListener('click', function (e) {
-        // body
-        e.preventDefault();
-        const pid=lnk.getAttribute("data-vs-pd");
-        console.log(pid);
-        const values=pid.split(",");
-        const title=values[0];
-        const detail=values[1];
-        const Progress = values[2];
 
-        let titleInput = document.getElementById('showtxttitle');
-        let detailInput=document.getElementById('showtxtDetail');
-        let progressInput=document.getElementById('showprogress');
+  document.querySelectorAll(".edit-Card").forEach((icon) => {
+    icon.addEventListener("click", function (e) {
+      e.stopPropagation(); // Stop event propagation
+      const pid = e.target.getAttribute("id");
+      const found = handler.FindProducts(pid);
+      if (found) {
+        document.getElementById("edithdnId").value = found.Id;
+        document.getElementById("edittxttitle").value = found.Title;
+        document.getElementById("edittxtDetail").value = found.Detail;
+        document.getElementById("editprogress").value = found.Category.Id;
 
-        titleInput.value=title;
-        detailInput.value=detail;
-        progressInput.value=Progress;
-        shModal.show();
+        editModal.show();
+      }
     });
-});
+  });
+
+
+
+  document.getElementById("btnedit").addEventListener("click", function () {
+    const editid = document.getElementById("edithdnId").value;
+    const edittitle = document.getElementById("edittxttitle").value;
+    const editdetail = document.getElementById("edittxtDetail").value;
+    const editprogress = document.getElementById("editprogress").value;
+
+    handler.EditTasks(editid, edittitle, editdetail, editprogress);
+    LoadCards(handler.GetRecord());
+    editModal.hide();
+  });
+
+
+
+
+  document.querySelectorAll('.card').forEach((card) => {
+    card.addEventListener('click', function () {
+      const cardTitle = card.querySelector('.link-show').textContent;
+      const cardDate = card.querySelector('.text-body-secondary').textContent;
+      const cardDetail = card.querySelector('.card-text').textContent;
+
+      // Populate modal with card content
+      document.getElementById('modalTitleId').textContent = cardTitle;
+      document.getElementById('showtxttitle').value = cardTitle;
+      document.getElementById('showtxtDetail').value = cardDetail;
+
+      // Open the modal
+      shModal.show();
+    });
+  });
+
 }
 
 document.getElementById('btnshow').addEventListener('click', function (e) {
-    // body
-    // e.preventDefault();
-    // const showid = document.getElementById("showhdnid").value;
-    // const showtitle = document.getElementById("showtxttitle").value;
-    // const showdetail = document.getElementById("showtxtDetail").value;
-    // const showprogress = document.getElementById("showprogress").value;
-    // handler.ShowTasks(showid, showtitle, showdetail, showprogress);
-    // LoadCards(handler.GetRecord());
-    shModal.hide();
+
+  shModal.hide();
 });
 
 
-document.getElementById("btndelete").addEventListener("click",function  () {
-    // body
-    const delid = document.getElementById("delhdnid").value;
-    const deltitle = document.getElementById("deltxttitle").value;
-    const deldetail = document.getElementById("deltxtDetail").value;
-    const delprogress = document.getElementById("delprogress").value;
-    handler.DeleteTasks(delid, deltitle, deldetail, delprogress);
-    LoadCards(handler.GetRecord());
-    deleteModal.hide();
+document.getElementById("btndelete").addEventListener("click", function () {
+  // body
+  const delid = document.getElementById("delhdnid").value;
+  const deltitle = document.getElementById("deltxttitle").value;
+  const deldetail = document.getElementById("deltxtDetail").value;
+  const delprogress = document.getElementById("delprogress").value;
+  handler.DeleteTasks(delid, deltitle, deldetail, delprogress);
+  LoadCards(handler.GetRecord());
+  deleteModal.hide();
 
 });
 
-// Favourite
-function favFun(params) {
-  let fav = document.querySelectorAll(".favourite");
-  fav.forEach((e) => {
-    e.addEventListener("click", () => {
-      // console.log(e.getAttribute("id"));
-      handler.setFavourite(e.getAttribute("id"));
-    });
-  });
-}
 
-document.getElementById("favourite").addEventListener("click", (e) => {
-  let tasks = handler.getFavourite();
-  // console.log(tasks);
-  LoadCards(tasks);
-});     
+
+
+// // Favourite
+// function favFun(params) {
+//   let fav = document.querySelectorAll(".favourite");
+//   fav.forEach((e) => {
+//     e.addEventListener("click", () => {
+//       handler.setFavourite(e.getAttribute("id"));
+//     });
+//   });
+// }
+
+// document.getElementById("favourite").addEventListener("click", (e) => {
+//   let tasks = handler.getFavourite();
+//   // console.log(tasks);
+//   LoadCards(tasks);
+// });
 
 // load Categories
 addEventListener("load", function () {
@@ -173,19 +196,27 @@ addEventListener("load", function () {
 });
 
 addEventListener("load", function () {
-    document.getElementById("delprogress").innerHTML = `<option value="0">Category</option>`;
-    for (let i = 0; i < category.length; i++) {
-      let temp = `<option value="${category[i].Id}">${category[i].Name}</option>`;
-      document.getElementById("delprogress").innerHTML += temp;
-    }
-  });
+  document.getElementById("delprogress").innerHTML = `<option value="0">Category</option>`;
+  for (let i = 0; i < category.length; i++) {
+    let temp = `<option value="${category[i].Id}">${category[i].Name}</option>`;
+    document.getElementById("delprogress").innerHTML += temp;
+  }
+});
 
-  addEventListener("load", function () {
-    document.getElementById("showprogress").innerHTML = `<option value="0">Category</option>`;
-    for (let i = 0; i < category.length; i++) {
-      let temp = `<option value="${category[i].Id}">${category[i].Name}</option>`;
-      document.getElementById("showprogress").innerHTML += temp;
-    }
-  });
+addEventListener("load", function () {
+  document.getElementById("editprogress").innerHTML = `<option value="0">Category</option>`;
+  for (let i = 0; i < category.length; i++) {
+    let temp = `<option value="${category[i].Id}">${category[i].Name}</option>`;
+    document.getElementById("editprogress").innerHTML += temp;
+  }
+});
+
+addEventListener("load", function () {
+  document.getElementById("showprogress").innerHTML = `<option value="0">Category</option>`;
+  for (let i = 0; i < category.length; i++) {
+    let temp = `<option value="${category[i].Id}">${category[i].Name}</option>`;
+    document.getElementById("showprogress").innerHTML += temp;
+  }
+});
 
 favFun();
